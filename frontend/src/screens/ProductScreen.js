40f30782a54cc84f4eabe41b9f2b064d467f7471
stan <amare605@gmail.com>
2022-09-30 10:34:48 +0800

    初版 productScreen 商品明細頁

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,55 @@
+import { Link, useParams } from 'react-router-dom'
 
-function ProductScreen() {
+import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
+import products from '../products'
+
+function ProductScreen({match}) {
+  const { id } = useParams();
+  const product = products.find((p) => String(p._id) === id);
+
+  if (!product) return null ; // 或跳到回去ui
+
   return (
-    <div>ProductScreen</div>
+    <>
+        <Link className='btn btn-primary my-3'  to="/">回上一頁</Link>
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
+              <ListGroup variant='flush'>
+                  <ListGroup.Item>
+                      <h3>{product.artist}</h3>
+                  </ListGroup.Item>
+              </ListGroup>
+              <ListGroup>
+                  <ListGroup.Item>
+                      <h3>{product.name}</h3>
+                  </ListGroup.Item>
+              </ListGroup>
+              <ListGroup>
+                  <ListGroup.Item>
+                      <h3>類別: {product.category}</h3>
+                  </ListGroup.Item>
+              </ListGroup>
+              <ListGroup>
+                  <ListGroup.Item>
+                      <h3>價格: NTD {product.price}</h3>
+                  </ListGroup.Item>
+              </ListGroup>
+              <ListGroup>
+                  <ListGroup.Item>
+                      <h3>庫存量: {product.countInStock > 0 ? product.countInStock: "此商品無庫存"}</h3>
+                  </ListGroup.Item>
+              </ListGroup>
+          </Col>
+          <Col md={3}>
+                  <button className='btn btn-block' type='button' disabled={product.countInStock <= 0}>
+                    <h3>加入購物車</h3>
+                  </button>       
+          </Col>
+        </Row>
+    </>
   )
 }
 
