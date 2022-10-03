f42d4e28aec5f694010c566fd3e436f948729003
stan <amare605@gmail.com>
2022-10-03 11:34:22 +0800

    sample data

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,5 @@
 const products = [
   {
-    _id: '001',
     name: 'Revelation 啓示錄 (正式版2CD)',
     image: '/images/001.jpg',
     artist: 'G.E.M. 鄧紫棋',
@@ -9,7 +8,6 @@ const products = [
     countInStock: 10,
   },
   {
-    _id: '002',
     name: 'Greatest Works Of Art 最偉大的作品',
     image: '/images/002.jpg',
     artist: 'JAY CHOU 周杰倫',
@@ -18,7 +16,6 @@ const products = [
     countInStock: 3,
   },
   {
-    _id: '003',
     name: 'Good Afternoon, Good Evening and Goodnight 明天再見',
     image: '/images/003.jpg',
     artist: 'WeiBird 韋禮安',
@@ -27,7 +24,6 @@ const products = [
     countInStock: 20,
   },
   {
-    _id: '004',
     name: '《馬拉美的星期二》',
     image: '/images/004.jpg',
     artist: 'Wu Qing Feng 吳青峰',
@@ -36,7 +32,6 @@ const products = [
     countInStock: 20,
   },
   {
-    _id: '005',
     name: 'Dawn Of Chromatica 神彩混音特輯',
     image: '/images/005.jpg',
     artist: 'LADY GAGA 女神卡卡',
@@ -45,7 +40,6 @@ const products = [
     countInStock: 10,
   },
   {
-    _id: '006',
     name: 'Red Taylor Version (2CD) 紅色 (泰勒絲全新版) 雙CD歐洲進口版',
     image: '/images/006.jpg',
     artist: 'TAYLOR SWIFT 泰勒絲',
