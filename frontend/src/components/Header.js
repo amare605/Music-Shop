6aa07af86f28caabdb268b83730c074ed3bf0489
stan <amare605@gmail.com>
2022-10-11 14:14:45 +0800

    Show User In Navbar & Logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,17 @@
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../actions/userActions'
 
 function Header() {
+  const dispatch = useDispatch()
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <header>
         <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
@@ -12,9 +22,21 @@ function Header() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                  <LinkContainer to="/login"> 
-                    <Nav.Link><i className="fa-solid fa-user">會員中心</i></Nav.Link>
-                  </LinkContainer>
+                  {userInfo ? (
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>會員資料</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      會員登出
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  ) :
+                    <LinkContainer to="/login"> 
+                      <Nav.Link><i className="fa-solid fa-user">會員中心</i></Nav.Link>
+                    </LinkContainer>
+                  }
+                 
                   <LinkContainer to="/cart"> 
                     <Nav.Link><i className="fa-solid fa-cart-shopping">購物車</i></Nav.Link>
                   </LinkContainer>      
