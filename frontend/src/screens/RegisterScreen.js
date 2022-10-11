849c86b146a9b0d94b8fcb06cb8c2bec03591b40
stan <amare605@gmail.com>
2022-10-11 16:12:24 +0800

    User Register Reducer, Action & Screen

---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,105 @@
+import { useState, useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+import { register } from '../actions/userActions'
+
+function RegisterScreen ({  history }) {
+  const location = useLocation();
+  
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [message, setMessage] = useState(null)
+
+  const dispatch = useDispatch()
+
+  const userRegister = useSelector((state) => state.userRegister)
+  const { loading, error, userInfo } = userRegister
+
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect)
+    }
+  }, [history, userInfo, redirect])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (password !== confirmPassword) {
+      setMessage('密碼不符')
+    } else {
+      dispatch(register(name, email, password))
+    }
+  }
+
+  return (
+    <FormContainer>
+      <h1>註冊會員</h1>
+      {message && <Message variant='danger'>{message}</Message>}
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='name'>
+          <Form.Label>名稱</Form.Label>
+          <Form.Control
+            type='name'
+            placeholder='請輸入姓名'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='email'>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='請輸入E-MAIL'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='password'>
+          <Form.Label>密碼</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='請輸入您的密碼'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='confirmPassword'>
+          <Form.Label>確認密碼</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='請再次輸入您的密碼'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type='submit' variant='primary'>
+          註冊會員
+        </Button>
+      </Form>
+
+      <Row className='py-3'>
+        <Col>
+          是否已有帳號?{' '}
+          <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+            登入
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  )
+}
+
+export default RegisterScreen
