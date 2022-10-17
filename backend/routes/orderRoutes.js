9be6c1d13a9408aaa681e0568af354cd6e6526bc
stan <amare605@gmail.com>
2022-10-17 16:27:36 +0800

    Order Controller & Route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 import {
   addOrderItems,
 } from '../controllers/orderController.js'
-import { protect, admin } from '../middleware/authMiddleware.js'
+import { protect  } from '../middleware/authMiddleware.js'
 
 
 router.route('/').post(protect, addOrderItems)
