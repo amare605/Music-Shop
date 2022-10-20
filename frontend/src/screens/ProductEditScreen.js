7d00f7078d1c1d42b1781eb4cab556c48f0c4461
stan <amare605@gmail.com>
2022-10-20 13:49:18 +0800

    Edit Product Screen

---
+++ frontend/src/screens/ProductEditScreen.js
@@ -0,0 +1,176 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+import { listProductsDetails, } from '../actions/productActions'
+//import { PRODUCT_UPDATE_RESET } from '../constants/productConstants'
+
+function ProductEditScreen () {
+  const {id} = useParams(); 
+  const productId = id
+
+  const navigate = useNavigate();
+
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState(0)
+  const [image, setImage] = useState('')
+  const [artist, setArtist] = useState('')
+  const [category, setCategory] = useState('')
+  const [countInStock, setCountInStock] = useState(0)
+  
+  const [uploading, setUploading] = useState(false)
+
+  const dispatch = useDispatch()
+
+  const productDetails = useSelector((state) => state.productDetails)
+  const { loading, error, product } = productDetails
+
+  //const productUpdate = useSelector((state) => state.productUpdate)
+  //const {
+    //loading: loadingUpdate,
+    //error: errorUpdate,
+    //success: successUpdate,
+  //} = productUpdate
+
+  useEffect(() => {
+    
+      if (!product.name || product._id !== productId) {
+        dispatch(listProductsDetails(productId))
+      } else {
+        setName(product.name)
+        setPrice(product.price)
+        setImage(product.image)
+        setArtist(product.artist)
+        setCategory(product.category)
+        setCountInStock(product.countInStock)
+      }
+    
+  }, [dispatch, navigate, productId, product, ])
+
+  //const uploadFileHandler = async (e) => {
+    //const file = e.target.files[0]
+    //const formData = new FormData()
+    //formData.append('image', file)
+    //setUploading(true)
+
+    //try {
+      //const config = {
+        //headers: {
+          //'Content-Type': 'multipart/form-data',
+        //},
+      //}
+
+      //const { data } = await axios.post('/api/upload', formData, config)
+
+      //setImage(data)
+      //setUploading(false)
+    //} catch (error) {
+      //console.error(error)
+      //setUploading(false)
+    //}
+  //}
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(
+      //updateProduct({
+        //_id: productId,
+        //name,
+        //price,
+        //image,
+        //artist,
+        //category,
+        //countInStock,
+      //})
+    )
+  }
+
+  return (
+    <>
+      <Link to='/admin/productlist' className='btn btn-light my-3'>
+        返回
+      </Link>
+      <FormContainer>
+        <h1>編輯商品</h1>
+        
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name'>
+              <Form.Label>專輯名稱</Form.Label>
+              <Form.Control
+                type='name'
+                placeholder='請輸入專輯名稱...'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='price'>
+              <Form.Label>價格</Form.Label>
+              <Form.Control
+                type='number'
+                placeholder='請輸入價格...'
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='image'>
+              <Form.Label>專輯圖片</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='請輸入專輯圖片 url...'
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='brand'>
+              <Form.Label>藝人</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='請輸入藝人...'
+                value={artist}
+                onChange={(e) => setArtist(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='countInStock'>
+              <Form.Label>庫存數</Form.Label>
+              <Form.Control
+                type='number'
+                placeholder='請輸入庫存數...'
+                value={countInStock}
+                onChange={(e) => setCountInStock(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='category'>
+              <Form.Label>類別</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='請輸入類別...'
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              ></Form.Control>
+            </Form.Group> 
+
+            <Button type='submit' variant='primary'>
+              更新
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  )
+}
+
+export default ProductEditScreen
